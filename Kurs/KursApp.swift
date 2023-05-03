74071b62361d48b2b74c213eec357ad7e47fca29
ansardauletbayev <dauletbayev.ansar@gmail.com>
2023-05-03 16:30:31 +0600

    first commit

---
+++ Kurs/KursApp.swift
@@ -0,0 +1,17 @@
+//
+//  KursApp.swift
+//  Kurs
+//
+//  Created by ANSAR DAULETBAYEV on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct KursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
